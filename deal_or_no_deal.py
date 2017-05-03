e4fbe04dd4b084ce2d8bdcbaa7fbc709e68f4d3f
Kathleen <kathleensdomingo@gmail.com>
2017-05-02 20:38:39 -0700

    first draft - pseudocode

---
+++ deal_or_no_deal.py
@@ -0,0 +1,73 @@
+# Deal or No Deal
+
+# Greet player with short intro about the game
+print "Welcome to Deal or No Deal"
+# show cash amounts
+prizes = [.01, 1, 10, 100, 1000, 10000, 50000, 100000, 500000, 1000000]
+
+# Ask to choose 1 out of 10 boxes with random cash amts
+# 10 boxes = .01, 1, 10, 100, 1000, 10000, 50000, 100000, 500000, 1000000
+# Ask player if she's sure about her choice (Y/N)
+# if sure, start game
+# if not, ask to choose again
+# if answer is not Y/N, show error message
+
+# Let's play!
+
+# Round 1: eliminate 4 boxes
+# Call Function B 4x
+### FUNCTION B: TO ELIMINATE AND REVEAL
+# ask to eliminate a box
+# reveal the cash amount
+
+### FUNCTION C: TO SHOW AVAILABLE PRIZES
+# show all the prizes not eliminated yet
+
+### FUNCTION D: computes the average of the remaining prizes
+# banker offers cash amount equivalent to the average of the remaining prizes
+
+### FUNCTION E: ask DEAL or NO DEAL
+# DEAL or NO DEAL
+# if player chooses DEAL, end game and print cash amount offered by banker
+# if player chooses NO DEAL, proceed to next round
+
+# Round 2: eliminate 3 boxes
+# Call Function B 3x
+### FUNCTION B
+# ask to eliminate a box
+# reveal the cash amount
+
+### FUNCTION C
+# show all the prizes not eliminated yet
+
+### FUNCTION D
+# banker offers cash equivalent to the ave of remaining prizes
+
+### FUNCTION E
+# DEAL or NO DEAL
+# if player chooses DEAL, end game and print cash amount offered by banker
+# if player chooses NO DEAL, proceed to next round
+
+# Round 3: eliminate 1 boxes 
+# Call function B 1x
+### FUNCTION B
+
+### FUNCTION C
+
+### FUNCTION D
+
+### FUNCTION E
+
+# Round 5 - FINAL ROUND: eliminate 1 box
+### FUNCTION F
+# ask if player wants to interchange the boxes
+# if yes, interchange
+# if no, continue
+# open box
+# player wins what's inside the box
+
+
+
+
+### There should be a list for remaining prizes
+
